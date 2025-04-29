f3f3b1a35d3d9d07eeb50760f510592c58640bd3
Habibur Rahman <imailhabibur@gmail.com>
2025-04-29 19:32:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('sidebarToggle');
-  const sidebar = document.getElementById('sidebar');
-  btn.addEventListener('click', () => sidebar.classList.toggle('active'));
-});
