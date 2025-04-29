bb3bde7a61ae0c0452757917b41f1511ef5d05d9
Habibur Rahman <imailhabibur@gmail.com>
2025-04-29 19:08:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('sidebarToggle');
+  const sidebar = document.getElementById('sidebar');
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
+});
